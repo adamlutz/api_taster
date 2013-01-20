0e4cd872ccfdd5663e8b4c2817c91f9335a9afae
gajananbhat <gajananbhat@yahoo.com>
2013-01-21 00:37:59 +0530

    Update app/assets/javascripts/api_taster/app.js

--- app/assets/javascripts/api_taster/app.js
+++ app/assets/javascripts/api_taster/app.js
@@ -173,6 +173,8 @@ jQuery(function($) {
 
     $("#show-api-response-div pre[ref=response-raw]").text(xhr.responseText);
 
+    $("#req-json").val(JSON.stringify(ApiTaster.serializeForm()));
+
     prettyPrint();
   }
 });
